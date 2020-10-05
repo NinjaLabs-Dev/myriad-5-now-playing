053cc78f880136ecb7cd327098405421f2d07939
NinjaLabs-Dev <fraser@ninjalabs.co.uk>
2020-10-05 11:32:31 +0100

    Added version control

--- utils.js
+++ utils.js
@@ -13,9 +13,17 @@ const https = require('https');
 const ghrepo = gclient.repo(getPackageName())
 
 checkForUpdate = () => {
-    ghrepo.releases(function(...args) {
-        console.log(args);
+    ghrepo.releases(function(_, data) {
+        let gitVersion = data[0].tag_name;
+        let gitUrl = data[0].html_url;
+        let localVersion = package.version;
+
+        if(localVersion < gitVersion) {
+            console.log(`[ INFO ] There's a new version available! ${gitVersion} - ${gitUrl}`)
+        }
     })
 }
+
+checkForUpdate()
 
 exports.checkForUpdate = checkForUpdate;
